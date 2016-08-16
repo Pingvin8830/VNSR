ea059ddb2f1b89e0e8f206c0cf1b086de98fae92
Vlasov Anton <Vlasov-Ant@yandex.ru>
2016-08-16 21:36:52 +0300

    Создание модели признаков дня

--- VNSR/calend_app/models.py
+++ VNSR/calend_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Signs (models.Model):
+	'''
+		Признаки дней
+	'''
+
+	class Meta ():
+		db_table = 'signs'
+
+	data    = models.DateField    (primary_key = True )
+	work    = models.BooleanField (default     = True )
+	week    = models.BooleanField (default     = False)
+	holiday = models.BooleanField (default     = False)
+	short   = models.BooleanField (default     = False)
+	comment = models.CharField    (max_length  = 100)
