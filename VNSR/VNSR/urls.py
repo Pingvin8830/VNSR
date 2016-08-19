2810cd3d2c43f7063dc6d2948e8630f5b9716217
Vlasov Anton <Vlasov-Ant@yandex.ru>
2016-08-20 02:19:39 +0300

    Перенапраление на страницу авторизации

--- VNSR/VNSR/urls.py
+++ VNSR/VNSR/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url,   include
 from django.contrib   import admin
+from .                import views
 
 urlpatterns = [
 	url (r'^admin/',  admin.site.urls),
 	url (r'^calend/', include ('calend_app.urls')),
 	url (r'^auth/',   include ('auth_app.urls')),
-	url (r'^',        include ('calend_app.urls')),
+	url (r'^',        views.case_page),
 ]
