1d658c9cb226132b9846417eaee36345c3e4ce23
Vlasov Anton <Vlasov-Ant@yandex.ru>
2016-08-20 01:45:40 +0300

    Добавление представлений авторизации и выхода

--- VNSR/auth_app/views.py
+++ VNSR/auth_app/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib   import auth
 
 # Create your views here.
+
+def login (request):
+	'''
+		Авторизация
+	'''
+	if request.POST:
+		username = request.POST ['username']
+		password = request.POST ['password']
+		user     = auth.authenticate (username = username, password = password)
+		if user is not None:
+			auth.login (request, user)
+			return redirect ('/')
+		else:
+			return render (request, 'auth/login.html', {})
+	else:
+		return render (request, 'auth/login.html', {})
+
+def logout (request):
+	auth.logout (request)
+	return redirect ('/')
+
