6ba3cdaa866af3e2036a1975db2266e8554b1cce
Vlasov Anton <Vlasov-Ant@yandex.ru>
2016-08-21 01:46:08 +0300

    Создание роутера БД baltbank

--- VNSR/routers.py
+++ VNSR/routers.py
@@ -32,6 +32,32 @@ class CalendRouter (object):
 			return db == 'calend_db'
 		return None
 
+class BaltbankRouter (object):
+	'''
+		Перенаправление в БД baltbank
+	'''
+
+	def db_for_read (self, model, **hints):
+		if model._meta.app_label == 'baltbank_app':
+			return 'baltbank_db'
+		return None
+
+	def db_for_write (self, model, **hints):
+		if model._meta.app_label == 'baltbank_app':
+			return 'baltbank_db'
+		return None
+
+	def allow_relation (self, obj1, obj2, **hints):
+		if	obj1._meta.app_label == 'baltbank_app' or \
+				obj2._meta.app_label == 'baltbank_app':
+					return True
+		return None
+
+	def allow_migrate (self, db, app_label, model = None, **hints):
+		if app_label == 'baltbank_app':
+			return db == 'baltbank_db'
+		return None
+
 class DefaultRouter (object):
 	'''
 		Перенаправление в БД default
