9a67329075ae7911647d43e3100b88dab857e307
Vlasov Anton <Vlasov-Ant@yandex.ru>
2016-08-21 15:30:00 +0300

    Создание основных функций

--- VNSR/main_app/views.py
+++ VNSR/main_app/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib   import auth
+
+def is_user (request):
+	'''
+		Проверка авторизации
+	'''
+	return auth.get_user (request).username != ''
+
+def default_context ():
+	'''
+		Создаёт контекст для шаблона сайта по умолчанию
+	'''
+	context = {}
+	return context
 
 # Create your views here.
+def index (request):
+	'''
+		Отображение стартовой страницы
+	'''
+	if not is_user (request):	return redirect ('/auth/login/')
+	page    = 'main/index.html'
+	context = default_context ()
+	return render (request, page, context)
