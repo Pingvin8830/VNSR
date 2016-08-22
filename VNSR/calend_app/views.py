e8e177cd62f683ac777ceee8673d13dc06cba6b5
Vlasov Anton <Vlasov-Ant@yandex.ru>
2016-08-22 15:13:41 +0300

    Добавление в представление вывода признаков дней месяца формы настройки

--- VNSR/calend_app/views.py
+++ VNSR/calend_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .functions       import create_calend_month, get_now
+from .functions       import create_calend_month, get_now, get_month_text, create_month_signs_form
 from main_app.views   import is_user, default_context
 
 # Create your views here.
@@ -22,10 +22,21 @@ def display_signs_month (request, year, month):
 	'''
 		Отображает настройку дней месяца
 	'''
-	year  = int (year)
-	month = int (month)
-	page  = 'calend/month.html'
+	if not is_user: return redirect ('/')
+	year    = int (year)
+	month   = int (month)
+	page    = 'calend/month.html'
 	context = default_context (request, 'app', 'calend_app')
-	context ['calend_year'] = year
-	context ['calend_month'] = month
+	context ['form_signs']        = create_month_signs_form (year, month, 1)
+	context ['form_signs']       += create_month_signs_form (year, month, 2)
+	context ['calend_year']       = year
+	context ['calend_month']      = month
+	context ['calend_month_text'] = get_month_text (year, month)
 	return render (request, page, context)
+
+def set_signs_month (request, year, month):
+	'''
+		Сохраняет выбранные признаки дней месяца в БД
+	'''
+	if not is_user: return redirect ('/')
+	return redirect ('/calend/%s' % (year))
