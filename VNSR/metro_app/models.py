1c5ba1301d030b9b77ee7edf1c3e600ae62a9207
http <http@localhost.localdomain>
2016-10-25 11:45:37 +0300

    Добавление почасовой ставки и стоимости обеда.

--- VNSR/metro_app/models.py
+++ VNSR/metro_app/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
 class WorkPlane (models.Model):
 	'''План смен'''
 	class Meta ():
-		db_table = 'work_plane'
+		db_table        = 'work_plane'
 		unique_together = (('date_start', 'code'))
 
 	id          = models.AutoField (primary_key = True)
@@ -20,20 +21,41 @@ class ShedulePlane (models.Model):
 	'''Планируемый график'''
 	class Meta ():
 		db_table = 'shedule_plane'
-	data  = models.DateField (primary_key = True)
+	id    = models.AutoField (primary_key = True)
+	data  = models.DateField (unique      = True)
 	shift = models.CharField (max_length  = 5)
 
 class SheduleReal (models.Model):
-	'''
-		Реальный график
-	'''
+	'''Реальный график'''
 	class Meta ():
 		db_table = 'shedule_real'
-	data        = models.DateField         (primary_key = True)
-	start       = models.TimeField         ()
-	end         = models.TimeField         ()
-	break_day   = models.SmallIntegerField (default = 2)
-	break_night = models.SmallIntegerField (default = 0)
-	delay       = models.TimeField         (default = '00:00')
+	id          = models.AutoField         (primary_key = True      )
+	data        = models.DateField         (unique      = True      )
+	start       = models.TimeField         (                   null = True)
+	end         = models.TimeField         (                   null = True)
+	break_day   = models.SmallIntegerField (default = 2,       null = True)
+	break_night = models.SmallIntegerField (default = 0,       null = True)
+	delay       = models.TimeField         (default = '00:00', null = True)
 	vacation    = models.BooleanField      (default = False)
 	sick        = models.BooleanField      (default = False)
+
+class Lanch (models.Model):
+	'''Стоимость обеда'''
+	class Meta ():
+		db_table        = 'lanch'
+		unique_together = (('start', 'end'))
+	id    = models.AutoField    (primary_key = True)
+	start = models.DateField    ()
+	end   = models.DateField    (null        = True)
+	value = models.DecimalField (max_digits  = 9, decimal_places = 4)
+
+class Rate (models.Model):
+	'''Почасовая ставка'''
+	class Meta ():
+		db_table        = 'rate'
+		unique_together = (('start', 'end'))
+	id    = models.AutoField    (primary_key = True)
+	start = models.DateField    ()
+	end   = models.DateField    (null        = True)
+	value = models.DecimalField (max_digits  = 9, decimal_places = 4)
+
