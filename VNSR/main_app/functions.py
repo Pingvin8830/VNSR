a4715282e1616ee8917b7880ab6978a5544a2a6a
Vlasov Anton <Vlasov-Ant@yandex.ru>
2016-12-07 21:07:57 +0300

    Замена табуляций

--- VNSR/main_app/functions.py
+++ VNSR/main_app/functions.py
@@ -3,15 +3,13 @@ from calend_app.functions import get_month_text, create_calend_month, get_now
 from menu_app.functions   import create_menu_user, create_menu_app
 
 def default_context (request, app = None):
-	'''
-		Создаёт контекст для шаблона сайта по умолчанию
-	'''
-	username = auth.get_user (request).username
-	context = {
-		'username':   username,
-		'month_text': get_month_text (),
-		'calend':     create_calend_month (),
-		'now':        get_now (),
-	}
-	context ['items'] = create_menu_user (username)
-	return context
+  '''Создаёт контекст для шаблона сайта по умолчанию'''
+  username = auth.get_user (request).username
+  context = {
+    'username':   username,
+    'month_text': get_month_text (),
+    'calend':     create_calend_month (),
+    'now':        get_now (),
+  }
+  context ['items'] = create_menu_user (username)
+  return context
