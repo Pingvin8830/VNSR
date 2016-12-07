bbc7cde60102565ab75d0244cb82657dfb540413
Vlasov Anton <Vlasov-Ant@yandex.ru>
2016-12-07 20:41:58 +0300

    Замена табуляций

--- VNSR/auth_app/views.py
+++ VNSR/auth_app/views.py
@@ -4,25 +4,22 @@ from django.contrib   import auth
 # Create your views here.
 
 def login (request):
-	'''
-		Авторизация
-	'''
-	if request.POST:
-		username = request.POST ['username']
-		password = request.POST ['password']
-		user     = auth.authenticate (username = username, password = password)
-		if user is not None:
-			auth.login (request, user)
-			return redirect ('/')
-		else:
-			return render (request, 'auth/login.html', {})
-	else:
-		return render (request, 'auth/login.html', {})
+  '''Авторизация'''
+
+  if request.POST:
+    username = request.POST ['username']
+    password = request.POST ['password']
+    user     = auth.authenticate (username = username, password = password)
+    if user is not None:
+      auth.login (request, user)
+      return redirect ('/')
+    else:
+      return render (request, 'auth/login.html', {})
+  else:
+    return render (request, 'auth/login.html', {})
 
 def logout (request):
-	'''
-		Выход
-	'''
-	auth.logout (request)
-	return redirect ('/')
+  '''Выход'''
+  auth.logout (request)
+  return redirect ('/')
 
