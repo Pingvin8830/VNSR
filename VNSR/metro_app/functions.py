a263ff3cf39c1ba1e77d4865816b26d7069cf412
Vlasov Anton <Vlasov-Ant@yandex.ru>
2016-12-09 20:01:35 +0300

    Добавление функции проверки периода детализации расчетного листка

--- VNSR/metro_app/functions.py
+++ VNSR/metro_app/functions.py
@@ -7,7 +7,8 @@ def decimal_to_money (count, decimal = 2, comment = '', delimiter = ','):
   fnd       = count.find ('.')
   length    = len (count)
   count_int = count [:fnd]
-  count_dec = count [fnd + 1:fnd + decimal]
+  fnd += 1
+  count_dec = count [fnd:fnd + decimal]
   count_new = ''
   length    = len (count_int)
   num       = 0
@@ -27,3 +28,14 @@ def decimal_to_money (count, decimal = 2, comment = '', delimiter = ','):
       count_new += '0'
   count_new += comment
   return count_new
+
+def control_period (control_period, test_period, uniq = False):
+  '''Проверка периода в детализации расчетного листка'''
+  res = True
+  control_date = control_period.year * 12 + control_period.month
+  test_date    = test_period.year    * 12 + test_period.month
+  if uniq:
+    if control_date != test_date: res = False
+  else:
+    if (control_date - test_date > 1) or (control_date - test_date < 0): res = False
+  return res
