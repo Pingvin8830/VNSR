d47ea399cd2e735179003adb678cef8fc535772c
Vlasov Anton <Vlasov-Ant@yandex.ru>
2017-01-07 12:56:28 +0300

    Регистрация моделей metro_app в административной части

--- VNSR/metro_app/admin.py
+++ VNSR/metro_app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models        import *
 
 # Register your models here.
+admin.site.register (CodesPayslip)
+admin.site.register (Lanch)
+admin.site.register (Payslip)
+admin.site.register (PayslipDetails)
+admin.site.register (Rate)
+admin.site.register (ShedulePlane)
+admin.site.register (SheduleReal)
+admin.site.register (WorkPlane)
