afd5338a19e570485f46b8f17f06f3bb0ec6ae7d
Vlasov Anton <Vlasov-Ant@yandex.ru>
2017-02-05 01:00:42 +0300

    Создание url для добавления организации

--- VNSR/budget_app/urls.py
+++ VNSR/budget_app/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
   url (r'^add_card',            views.add_card,            name = 'add_card'),
   url (r'^add_debet_type',      views.add_debet_type,      name = 'add_debet_type'),
   url (r'^add_org_type',        views.add_org_type,        name = 'add_org_type'),
+  url (r'^add_org',             views.add_org,             name = 'add_org'),
   url (r'^display_cards',       views.display_cards,       name = 'display_cards'),
   url (r'^display_debet_types', views.display_debet_types, name = 'display_debet_types'),
   url (r'^display_org_types',   views.display_org_types,   name = 'display_org_types'),
