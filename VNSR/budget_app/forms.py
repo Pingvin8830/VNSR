eab9117e75467f8b0a04e7657295e3963ddce89e
Vlasov Anton <Vlasov-Ant@yandex.ru>
2017-02-05 00:59:24 +0300

    Создание формы добавления организации

--- VNSR/budget_app/forms.py
+++ VNSR/budget_app/forms.py
@@ -1,5 +1,5 @@
 from django  import forms
-from .models import Cards, DebetTypes, OrgTypes
+from .models import Cards, DebetTypes, Orgs, OrgTypes
 
 # Create your forms here.
 
@@ -21,6 +21,21 @@ class AddDebetTypeForm (forms.ModelForm):
 
   name    = forms.CharField (label = 'Название',    max_length = 50)
   comment = forms.CharField (label = 'Комментарий', max_length = 100, required = False)
+
+class AddOrgForm (forms.ModelForm):
+  '''Организации'''
+  class Meta ():
+    model = Orgs
+    fields = ['name', 'city', 'street', 'house', 'build', 'flat', 'phone', 'type']
+
+  name   = forms.CharField        (label = 'Название / ФИО',  max_length = 100)
+  city   = forms.CharField        (label = 'Город',           max_length = 50, required = False)
+  street = forms.CharField        (label = 'Улица',           max_length = 50, required = False)
+  house  = forms.IntegerField     (label = 'Дом',             min_value  = 0,  required = False)
+  build  = forms.CharField        (label = 'Корпус / Литера', max_length = 2,  required = False)
+  flat   = forms.IntegerField     (label = 'Квартира',        min_value  = 0,  required = False)
+  phone  = forms.CharField        (label = 'Телефон',         max_length = 20, required = False)
+  type   = forms.ModelChoiceField (label = 'Тип', queryset = OrgTypes.objects.all ())
 
 class AddOrgTypeForm (forms.ModelForm):
   '''Типы организаций'''
