df6792b96fe1dd148669e3ad94058cf56d5461ef
Vlasov Anton <Vlasov-Ant@yandex.ru>
2017-02-04 23:07:26 +0300

    Создание формы для добавления типов дохода

--- VNSR/budget_app/forms.py
+++ VNSR/budget_app/forms.py
@@ -1,22 +1,31 @@
 from django  import forms
-from .models import Cards, OrgTypes
+from .models import Cards, DebetTypes, OrgTypes
 
 # Create your forms here.
 
 class AddCardForm (forms.ModelForm):
   '''Счета хранения средств'''
   class Meta ():
-    model = Cards
+    model  = Cards
     fields = ['number', 'name', 'comment']
 
   number  = forms.CharField (label = 'Номер карточки', max_length = 20, required = False)
   name    = forms.CharField (label = 'Название',       max_length = 20)
   comment = forms.CharField (label = 'Комментарий',    max_length = 100, required = False)
 
+class AddDebetTypeForm (forms.ModelForm):
+  '''Типы дохода'''
+  class Meta ():
+    model  = DebetTypes
+    fields = ['name', 'comment']
+
+  name    = forms.CharField (label = 'Название',    max_length = 50)
+  comment = forms.CharField (label = 'Комментарий', max_length = 100, required = False)
+
 class AddOrgTypeForm (forms.ModelForm):
   '''Типы организаций'''
   class Meta ():
-    model = OrgTypes
+    model  = OrgTypes
     fields = ['name', 'comment']
 
   name    = forms.CharField (label = 'Название',    max_length = 50)
