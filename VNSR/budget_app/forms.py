0ec6b8cda34e0a261306055ba7abfd9f001ab52a
Vlasov Anton <Vlasov-Ant@yandex.ru>
2017-02-05 09:29:41 +0300

    Создание формы выбора периода

--- VNSR/budget_app/forms.py
+++ VNSR/budget_app/forms.py
@@ -1,6 +1,55 @@
 from django  import forms
 from .models import Cards, Debets, DebetTypes, Orgs, OrgTypes
 
+MONTHS = (
+  ( 1, 'Январь'),
+  ( 2, 'Февраль'),
+  ( 3, 'Март'),
+  ( 4, 'Апрель'),
+  ( 5, 'Май'),
+  ( 6, 'Июнь'),
+  ( 7, 'Июль'),
+  ( 8, 'Август'),
+  ( 9, 'Сентябрь'),
+  (10, 'Октябрь'),
+  (11, 'Ноябрь'),
+  (12, 'Декабрь'),
+)
+
+DAYS = (
+  ( 1,  '1'),
+  ( 2,  '2'),
+  ( 3,  '3'),
+  ( 4,  '4'),
+  ( 5,  '5'),
+  ( 6,  '6'),
+  ( 7,  '7'),
+  ( 8,  '8'),
+  ( 9,  '9'),
+  (10, '10'),
+  (11, '11'),
+  (12, '12'),
+  (13, '13'),
+  (14, '14'),
+  (15, '15'),
+  (16, '16'),
+  (17, '17'),
+  (18, '18'),
+  (19, '19'),
+  (20, '20'),
+  (21, '21'),
+  (22, '22'),
+  (23, '23'),
+  (24, '24'),
+  (25, '25'),
+  (26, '26'),
+  (27, '27'),
+  (28, '28'),
+  (29, '29'),
+  (30, '30'),
+  (31, '31'),
+)
+
 # Create your forms here.
 
 class AddCardForm (forms.ModelForm):
@@ -59,4 +108,14 @@ class AddOrgTypeForm (forms.ModelForm):
 
   name    = forms.CharField (label = 'Название',    max_length = 50)
   comment = forms.CharField (label = 'Комментарий', max_length = 100, required = False)
+
+class CasePeriodForm (forms.Form):
+  '''Выбор периода'''
+
+  year_start  = forms.IntegerField (label = 'Год',   min_value = 0,  max_value = 9999, initial = 2017)
+  year_end    = forms.IntegerField (label = 'Год',   min_value = 0,  max_value = 9999, initial = 2017)
+  month_start = forms.ChoiceField  (label = 'Месяц', choices   = MONTHS)
+  month_end   = forms.ChoiceField  (label = 'Месяц', choices   = MONTHS)
+  day_start   = forms.ChoiceField  (label = 'День',  choices   = DAYS)
+  day_end     = forms.ChoiceField  (label = 'День',  choices   = DAYS)
 
