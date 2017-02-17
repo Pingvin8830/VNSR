95fc8ead0defac5b4eb1609f20bfc4cc34d3dbb0
Vlasov Anton <Vlasov-Ant@yandex.ru>
2017-02-17 11:26:14 +0300

    Доработка моделей для расходов

--- VNSR/budget_app/models.py
+++ VNSR/budget_app/models.py
@@ -49,15 +49,17 @@ class Orgs (models.Model):
   def __str__ (self):
     return '%s' % self.name
 
-  id     = models.AutoField            (primary_key = True)
-  name   = models.CharField            (null = False, max_length = 100)
-  city   = models.CharField            (null = True,  max_length = 50)
-  street = models.CharField            (null = True,  max_length = 50)
-  house  = models.PositiveIntegerField (null = True)
-  build  = models.CharField            (null = True,  max_length = 2)
-  flat   = models.PositiveIntegerField (null = True)
-  phone  = models.CharField            (null = True,  max_length = 50)
-  type   = models.ForeignKey           ('OrgTypes',   db_column = 'type', null = False, on_delete = models.SET_DEFAULT, default = -1)
+  id      = models.AutoField            (primary_key = True)
+  name    = models.CharField            (null = False, max_length = 100)
+  region  = models.CharField            (null = True,  max_length = 100, default = 'СПб')
+  city    = models.CharField            (null = True,  max_length = 50)
+  street  = models.CharField            (null = True,  max_length = 50)
+  house   = models.PositiveIntegerField (null = True)
+  build   = models.CharField            (null = True,  max_length = 2)
+  flat    = models.PositiveIntegerField (null = True)
+  phone   = models.CharField            (null = True,  max_length = 50)
+  comment = models.CharField            (null = True,  max_length = 100)
+  type    = models.ForeignKey           ('OrgTypes',   db_column = 'type', null = False, on_delete = models.SET_DEFAULT, default = -1)
 
 class OrgTypes (models.Model):
   '''Типы организаций'''
@@ -79,6 +81,7 @@ class Credits (models.Model):
   id      = models.AutoField    (primary_key = True) 
   price   = models.DecimalField (null = False, max_digits = 9, decimal_places = 4)
   count   = models.DecimalField (null = False, max_digits = 9, decimal_places = 4)
+  cost    = models.DecimalField (null = True,  max_digits = 9, decimal_places = 4, default = 0)
   is_need = models.BooleanField (default = True)
   product = models.ForeignKey   ('Products',   db_column = 'product', null = False, on_delete = models.SET_DEFAULT, default = -1)
   cheque  = models.ForeignKey   ('Cheques',    db_column = 'cheque',  null = False, on_delete = models.SET_DEFAULT, default = -1)
@@ -110,10 +113,15 @@ class Cheques (models.Model):
   class Meta (object):
     db_table = 'cheques'
 
-  id     = models.AutoField  (primary_key = True)
-  number = models.CharField  (null = True, max_length = 5)
-  date   = models.DateField  (null = False)
-  time   = models.TimeField  (null = True)
-  org    = models.ForeignKey ('Orgs',  db_column = 'org',  null = False, on_delete = models.SET_DEFAULT, default = -1)
-  card   = models.ForeignKey ('Cards', db_column = 'card', null = False, on_delete = models.SET_DEFAULT, default = -1)
+  def __str__ (self):
+    return '%s - %s' % (self.number, self.org.name)
+
+  id       = models.AutoField    (primary_key = True)
+  number   = models.CharField    (null = True, max_length = 5)
+  date     = models.DateField    (null = False)
+  time     = models.TimeField    (null = True)
+  kassa    = models.IntegerField (null = True)
+  discount = models.DecimalField (null = False, default = 0, max_digits = 9, decimal_places = 2)
+  org      = models.ForeignKey   ('Orgs',  db_column = 'org',  null = False, on_delete = models.SET_DEFAULT, default = -1)
+  card     = models.ForeignKey   ('Cards', db_column = 'card', null = False, on_delete = models.SET_DEFAULT, default = -1)
 
