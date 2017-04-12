cf911e928c976e772261e358b2871b3d7f126ced
Vlasov Anton <Vlasov-Ant@yandex.ru>
2017-04-12 21:43:28 +0300

    Создание представления для добавления чека с заправки

--- VNSR/car_app/views.py
+++ VNSR/car_app/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts   import render
-from main_app.views     import is_user, default_context
-from menu_app.functions import create_menu_app
+from datetime                           import datetime
+from django.shortcuts                   import render, redirect
+from django.template.context_processors import csrf
+from .forms                             import AddRefuelForm
+from main_app.views                     import is_user, default_context
+from menu_app.functions                 import create_menu_app
 
 # Create your views here.
 
@@ -11,4 +14,32 @@ def index (request):
   context = default_context (request)
   context ['items'] = create_menu_app ('car')
   return render (request, page, context)
+
+def add_refuel (request):
+  '''Добавление чека с заправки'''
+  if not is_user (request): return redirect ('/')
+  if request.POST:
+    form = AddRefuelForm (request.POST)
+    print ()
+    print (form.is_valid ())
+    print ()
+    if form.is_valid ():
+      data = form.cleaned_data
+      refuel = form.save (commit = False)
+      refuel.date_time = datetime (
+        int (data ['year']),
+        int (data ['month']),
+        int (data ['day']),
+        int (data ['hour']),
+        int (data ['minute']),
+        int (data ['second']),
+      )
+      refuel.save ()
+    return index (request)
+  else:
+    page    = 'car/add_refuel.html'
+    context = default_context (request)
+    context.update (csrf (request))
+    context ['form'] = AddRefuelForm
+    return render (request, page, context)
 
