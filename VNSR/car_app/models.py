1baf30e7c823dda7468af8e55caee7e6a19a427e
Vlasov Anton <Vlasov-Ant@yandex.ru>
2017-07-31 04:21:59 +0300

    Более правильное название поля в модели контрольных точек

--- VNSR/car_app/models.py
+++ VNSR/car_app/models.py
@@ -80,11 +80,11 @@ class Travels (models.Model):
 class CheckPoints (models.Model):
   '''Контрольные точки поездки'''
   class Meta (object):
-    db_table = 'checkpoints'
+    db_table = 'check_points'
 
   id            = models.AutoField            (primary_key = True)
   travel        = models.ForeignKey           ('Travels', on_delete = models.SET (-1), null = False, db_column = 'travel')
-  address       = models.CharField            (max_length = 100)
+  name          = models.CharField            (max_length = 100)
   date_time_in  = models.DateTimeField        (null = True)
   date_time_out = models.DateTimeField        (null = True)
   odometer      = models.PositiveIntegerField ()
