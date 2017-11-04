9f2192e82bfa9585a0386b0b7012cf77b01f7dd9
Vlasov Anton <Vlasov-Ant@yandex.ru>
2017-11-04 12:51:07 +0300

    Создание представления для отображения всех людей

--- VNSR/humans_app/views.py
+++ VNSR/humans_app/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts   import render, redirect
+from .models            import Humans
 from main_app.views     import is_user, default_context
 from menu_app.functions import create_menu_app
 
 # Create your views here.
+def display_all (request):
+  '''Отображение всех людей'''
+  if not is_user (request): return redirect ('/')
+  page = 'humans/display_all.html'
+  context = default_context (request)
+  context ['humans'] = Humans.objects.all ().order_by ('family', 'name', 'father_name')
+  return render (request, page, context)
+
 def index (request):
   '''Стартовое представление'''
   if not is_user (request): return redirect ('/')
