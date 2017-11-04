88f339a7da7139b95d4db5e576258c2f01b87725
Vlasov Anton <Vlasov-Ant@yandex.ru>
2017-11-04 12:18:38 +0300

    Создание стартового представления

--- VNSR/humans_app/views.py
+++ VNSR/humans_app/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts   import render, redirect
+from main_app.views     import is_user, default_context
+from menu_app.functions import create_menu_app
 
 # Create your views here.
+def index (request):
+  '''Стартовое представление'''
+  if not is_user (request): return redirect ('/')
+  page = 'humans/index.html'
+  context = default_context (request)
+  context ['items'] = create_menu_app ('humans')
+  return render (request, page, context)
+
