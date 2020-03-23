bb19a3a6be64109c168e2707f930a168141dd6a7
Pingvin8830 <vlasov-ant@yandex.ru>
2020-03-23 17:19:49 +0300

    Подгоняем под HTML 5

--- vnsr/vnsr/settings.py
+++ vnsr/vnsr/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
   'auth_app.apps.AuthAppConfig',
   'main.apps.MainConfig',
+  'tasks.apps.TasksConfig',
   'django.contrib.admin',
   'django.contrib.auth',
   'django.contrib.contenttypes',
