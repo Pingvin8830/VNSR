0e79ac7ad996be7b473f9f0f943b2a423140510f
Pingvin8830 <vlasov-ant@yandex.ru>
2020-04-08 14:45:28 +0300

    Добавил календарь месяца

--- vnsr/calend/views.py
+++ vnsr/calend/views.py
@@ -15,6 +15,50 @@ class Index(LoginRequiredMixin, generic.RedirectView):
   permanent = True
   url = reverse_lazy('calend:year', args=[datetime.today().year])
 
+class Month(LoginRequiredMixin, generic.TemplateView):
+  login_url = reverse_lazy('auth_app:login')
+  redirect_field_name = None
+  http_method_names = ['get']
+  template_name = 'calend/month.html'
+
+  def get_context_data(self, *args, **kwargs):
+    context = super().get_context_data(*args, **kwargs)
+    context['year'] = {
+      'prev': self.kwargs['year'],
+      'now': self.kwargs['year'],
+      'next': self.kwargs['year']
+    }
+    context['month'] = {
+      'prev': self.kwargs['month']-1,
+      'now': self.kwargs['month'],
+      'name': constants.MONTHS_TEXT[self.kwargs['month']],
+      'next': self.kwargs['month']+1
+    }
+    if not context['month']['prev']:
+      context['year']['prev'] -= 1
+      context['month']['prev'] = 12
+    if context['month']['next'] == 13:
+      context['year']['next'] += 1
+      context['month']['next'] = 1
+    context['days'] = []
+    for i in calendar.Calendar().itermonthdates(self.kwargs['year'], self.kwargs['month']):
+      if i.month == self.kwargs['month']:
+        day = {'num': i.day, 'weekday': constants.DAYS_TEXT[i.weekday()], 'signs': [], 'marks': []}
+        for mark in models.Marks.objects.filter(date=i):
+          if mark.sign not in day['signs']: day['signs'].append(mark.sign)
+          day['marks'].append(mark)
+        try:
+          date_type = models.Productions.objects.get(date=i).type
+        except models.Productions.DoesNotExist:
+          date_type = None
+        if date_type == 'В': day['type'] = 'week'
+        elif date_type == 'П': day['type'] = 'holiday'
+        elif date_type == 'С': day['type'] = 'short'
+        else: day['type'] = 'work'
+        context['days'].append(day)
+      pass
+    return context
+
 class SetProduction(LoginRequiredMixin, generic.TemplateView):
   login_url = reverse_lazy('auth_app:login')
   redirect_field_name = None
@@ -49,7 +93,7 @@ class SetProduction(LoginRequiredMixin, generic.TemplateView):
     context['days'] = []
     for day in calendar.Calendar().itermonthdates(self.kwargs['year'], self.kwargs['month']):
       if day.month == self.kwargs['month']:
-        day_context = { 'date': day.day }
+        day_context = { 'date': day.day, 'weekday': constants.DAYS_TEXT[day.weekday()] }
         try:
           day_context['type'] = models.Productions.objects.get(date=day).type
         except models.Productions.DoesNotExist:
@@ -92,62 +136,37 @@ class Year(LoginRequiredMixin, generic.TemplateView):
 
   def get_context_data(self, *args, **kwargs):
     context = super().get_context_data(*args, **kwargs)
-    calend = '<table id="year_calend">\n'
-    calend += '<caption>\n'
-    calend += '<a href="%s"><--</a>\n' % reverse_lazy('calend:year', args=[self.kwargs['year']-1])
-    calend += '%d\n' % self.kwargs['year']
-    calend += '<a href="%s">--></a>\n' % reverse_lazy('calend:year', args=[self.kwargs['year']+1])
-    calend += '</caption>\n'
-    kvartal = 0
-    for month in range(1, 13):
-      if month % 3 == 1:
-        kvartal += 1
-        calend += '<tr>\n'
-        calend += '<td>%d квартал</td>\n' % kvartal
-      calend += '<td>\n'
-      calend += self.get_month_html(self.kwargs['year'], month)
-      calend += '</td>\n'
-      if not month % 3:
-        calend += '</tr>\n'
-    calend += '</table>\n'
-    context['calend'] = calend
+    context['year'] = {
+      'prev': self.kwargs['year']-1,
+      'now': self.kwargs['year'],
+      'next': self.kwargs['year']+1
+    }
+    context['kvartals'] = []
+    for i in range(1, 5):
+      kvartal = {'num': i, 'months': []}
+      for j in range(i*3-2, i*3+1):
+        month = {
+          'num': j,
+          'name': constants.MONTHS_TEXT[j],
+          'days': []
+        }
+        for k in calendar.Calendar().itermonthdates(self.kwargs['year'], j):
+          day = {}
+          if k.month == j:
+            day['num'] = k.day
+            day['weekday'] = k.weekday()
+            try:
+              date_type = models.Productions.objects.get(date=k).type
+            except models.Productions.DoesNotExist:
+              date_type = None
+            if date_type == 'В':   day['type'] = 'week'
+            elif date_type == 'П': day['type'] = 'holiday'
+            elif date_type == 'С': day['type'] = 'short'
+            else:                  day['type'] = 'work'
+          else:
+            day['num'] = ''
+          month['days'].append(day)
+        kvartal['months'].append(month)
+      context['kvartals'].append(kvartal)
     return context
 
-  def get_month_html(self, year, month):
-    calend = calendar.Calendar()
-    res = '<table>\n'
-    res += '<caption>\n'
-    res += '%s\n' % constants.MONTHS_TEXT[month]
-    res += '</caption>\n' % constants.MONTHS_TEXT[month]
-    res += '<tr> '
-    res += '<th>Пн</th> '
-    res += '<th>Вт</th> '
-    res += '<th>Ср</th> '
-    res += '<th>Чт</th> '
-    res += '<th>Пт</th> '
-    res += '<th class="week">Сб</th> '
-    res += '<th class="week">Вс</th> '
-    res += '</tr>\n'
-    for day in calend.itermonthdays2(year, month):
-      if day[0]:
-        try:
-          date_type = models.Productions.objects.get(date=date(year, month, day[0])).type
-        except models.Productions.DoesNotExist:
-          date_type = None
-      if day[1] == 0:
-        res += '<tr> '
-      if day[0]:
-        if date_type == 'В':
-          res += '<td class="week right_text">%d</td> ' % day[0]
-        elif date_type == 'П':
-          res += '<td class="holiday right_text">%d</td> ' % day[0]
-        elif date_type == 'С':
-          res += '<td class="short right_text">%d</td> ' % day[0]
-        else:
-          res += '<td class="right_text">%d</td> ' % day[0]
-      else:
-        res += '<td>  </td> '
-      if day[1] == 6:
-        res += '</tr>\n'
-    res += '</table>\n'
-    return res
