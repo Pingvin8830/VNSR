6278da9f852e4a5e29f217ad7cdc9d1809dd19c1
anton <vlasov-ant@yandex.ru>
2024-03-18 00:56:18 +0300

    Register TravelState model in admin

--- vnsr/travels/admin.py
+++ vnsr/travels/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Travel
+from .models import Travel, TravelState
 
 # Register your models here.
+admin.site.register(TravelState)
 admin.site.register(Travel)
 
