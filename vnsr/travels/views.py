abc7e084568cb29438eaf897f5d9c85a6a683791
anton <vlasov-ant@yandex.ru>
2024-03-18 01:14:48 +0300

    Add render template for list travels

--- vnsr/travels/views.py
+++ vnsr/travels/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 from .models import Travel
 
 # Create your views here.
 def index(request):
   travels = Travel.objects.all()
-  output = ','.join([t.name for t in travels])
-  return HttpResponse(output)
+  template = loader.get_template('travels/index.html')
+  context = {
+    'travels': travels,
+  }
+  return HttpResponse(template.render(context, request))
 
 def detail(request, travel_id):
   return HttpResponse(f'Детализация поездки {travel_id}')
