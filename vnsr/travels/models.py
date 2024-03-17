337e789ce118e71aaf971f79168654d908012d5f
anton <vlasov-ant@yandex.ru>
2024-03-18 00:52:20 +0300

    Add verbose_name for fields

--- vnsr/travels/models.py
+++ vnsr/travels/models.py
@@ -7,7 +7,7 @@ class TravelStates(models.Model):
     verbose_name = 'Состояние поездки'
     verbose_name_plural = 'Состояния поездки'
 
-  name = models.CharField(max_length=20, unique=True)
+  name = models.CharField(max_length=20, unique=True, verbose_name='Название')
 
 class Place(models.Model):
   class Meta:
@@ -15,10 +15,10 @@ class Place(models.Model):
     verbose_name = 'Место'
     verbose_name_plural = 'Места'
 
-  name    = models.CharField(max_length=255)
-  city    = models.CharField(max_length=255)
-  address = models.TextField()
-  phones  = models.CharField(max_length=255)
+  name    = models.CharField(max_length=255, verbose_name='Название')
+  city    = models.CharField(max_length=255, verbose_name='Город')
+  address = models.TextField(verbose_name='Адрес')
+  phones  = models.CharField(max_length=255, verbose_name='Телефоны')
 
 class Travel(models.Model):
   class Meta:
@@ -27,10 +27,10 @@ class Travel(models.Model):
     verbose_name = 'Путешествие'
     verbose_name_plural = 'Путешествия'
 
-  name           = models.CharField(max_length=255, unique=True)
-  datetime_start = models.DateTimeField()
-  datetime_end   = models.DateTimeField()
-  points         = models.ManyToManyField(Place)
-  participants   = models.CharField(max_length=255)
-  state          = models.ForeignKey(TravelStates, on_delete=models.PROTECT)
+  name           = models.CharField(max_length=255, unique=True, verbose_name='Название')
+  datetime_start = models.DateTimeField(verbose_name='Дата и время начала')
+  datetime_end   = models.DateTimeField(verbose_name='Дата и время окончания')
+  points         = models.ManyToManyField(Place, blank=True, verbose_name='Путевые точки')
+  participants   = models.CharField(max_length=255, verbose_name='Участники')
+  state          = models.ForeignKey(TravelStates, on_delete=models.PROTECT, verbose_name='Состояние')
 
