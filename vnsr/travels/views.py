39be7ea0b20bc5880baa6b24302b7d1db28cbc96
anton <vlasov-ant@yandex.ru>
2024-03-18 01:16:39 +0300

    Remove HttpResponse, usage render

--- vnsr/travels/views.py
+++ vnsr/travels/views.py
@@ -1,17 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
 
 from .models import Travel
 
 # Create your views here.
 def index(request):
   travels = Travel.objects.all()
-  template = loader.get_template('travels/index.html')
   context = {
     'travels': travels,
   }
-  return HttpResponse(template.render(context, request))
+  return render(request, 'travels/index.html', context)
 
 def detail(request, travel_id):
   return HttpResponse(f'Детализация поездки {travel_id}')
