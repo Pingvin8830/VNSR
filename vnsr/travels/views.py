f4c315fca4c5fd74571901f106c62ea65c6fbe94
anton <vlasov-ant@yandex.ru>
2024-03-18 01:09:41 +0300

    RND index view for travels app.

--- vnsr/travels/views.py
+++ vnsr/travels/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Travel
+
 # Create your views here.
 def index(request):
-  return HttpResponse('Стартовая страница приложения "Путешествия"')
+  travels = Travel.objects.all()
+  output = ','.join([t.name for t in travels])
+  return HttpResponse(output)
 
 def detail(request, travel_id):
   return HttpResponse(f'Детализация поездки {travel_id}')
