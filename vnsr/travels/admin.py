9a279956e3b0091cfd8ec1abffd39b20fdae8d9d
anton <vlasov-ant@yandex.ru>
2024-03-18 14:13:10 +0300

    Register model TollRoad in admin

--- vnsr/travels/admin.py
+++ vnsr/travels/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Travel, TravelState, Point, Place, Way
+from .models import Travel, TravelState, Point, Place, Way, TollRoad
 
 # Register your models here.
 admin.site.register(Place)
@@ -8,3 +8,4 @@ admin.site.register(Point)
 admin.site.register(TravelState)
 admin.site.register(Travel)
 admin.site.register(Way)
+admin.site.register(TollRoad)
