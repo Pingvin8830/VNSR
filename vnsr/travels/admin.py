f3e2076b4172f8e4edbad8bbb9dbb22cc31078e6
anton <vlasov-ant@yandex.ru>
2024-03-18 12:20:43 +0300

    Register models Place and Point in admin

--- vnsr/travels/admin.py
+++ vnsr/travels/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
-from .models import Travel, TravelState
+from .models import Travel, TravelState, Point, Place
 
 # Register your models here.
+admin.site.register(Place)
+admin.site.register(Point)
 admin.site.register(TravelState)
 admin.site.register(Travel)
 
