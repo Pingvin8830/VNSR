a8acad6dae10bdc74ea08f56fb89fe76f72dbbd4
anton <vlasov-ant@yandex.ru>
2024-03-18 15:21:13 +0300

    Fix hard url to named pool

--- vnsr/travels/urls.py
+++ vnsr/travels/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'travels'
 urlpatterns = [
   path('', views.index, name='index'),
   path('<int:travel_id>/', views.detail, name='detail'),
