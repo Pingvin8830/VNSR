c9a6b875eac07186d8c54468fd0f549901ff4356
anton <vlasov-ant@yandex.ru>
2024-03-18 14:36:00 +0300

    Register model Hotel in admin

--- vnsr/travels/admin.py
+++ vnsr/travels/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Travel, TravelState, Point, Place, Way, TollRoad
+from .models import Travel, TravelState, Point, Place, Way, TollRoad, Hotel
 
 # Register your models here.
 admin.site.register(Place)
@@ -9,3 +9,4 @@ admin.site.register(TravelState)
 admin.site.register(Travel)
 admin.site.register(Way)
 admin.site.register(TollRoad)
+admin.site.register(Hotel)
