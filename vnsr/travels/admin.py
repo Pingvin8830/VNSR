1709137c5fecc4e14ee9bda6e773a87850f982f9
anton <vlasov-ant@yandex.ru>
2024-03-18 13:41:17 +0300

    Register model Way in admin

--- vnsr/travels/admin.py
+++ vnsr/travels/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 
-from .models import Travel, TravelState, Point, Place
+from .models import Travel, TravelState, Point, Place, Way
 
 # Register your models here.
 admin.site.register(Place)
 admin.site.register(Point)
 admin.site.register(TravelState)
 admin.site.register(Travel)
-
+admin.site.register(Way)
