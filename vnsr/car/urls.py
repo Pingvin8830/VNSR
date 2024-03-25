902e5336155f2931a9ee40a6eced5056b310df68
anton <vlasov-ant@yandex.ru>
2024-03-25 13:29:57 +0300

    Add url for refuel details

--- vnsr/car/urls.py
+++ vnsr/car/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 app_name = 'car'
 urlpatterns = [
+  path('refuels/<int:pk>', views.RefuelView.as_view(), name='refuel'),
   path('refuels', views.Refuels.as_view(), name='refuels'),
   path('', views.Index.as_view(), name='index'),
 ]
