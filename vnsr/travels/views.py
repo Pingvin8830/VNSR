239fea307ed76925bda9f8b9ec8cfb1595bfc38e
anton <vlasov-ant@yandex.ru>
2024-03-26 15:54:13 +0300

    Add context vars in travel

--- vnsr/travels/views.py
+++ vnsr/travels/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
 
+from vnsr.settings import DEBUG
 from .models import Travel
 
 # Create your views here.
@@ -12,8 +13,19 @@ class IndexView(LoginRequiredMixin, generic.ListView):
   def get_queryset(self):
     return Travel.objects.all()
 
+  def get_context_data(self, **kwargs):
+    context = super().get_context_data(**kwargs)
+    context['debug'] = DEBUG
+    context['active_app'] = 'travels'
+    return context
+
 class DetailView(LoginRequiredMixin, generic.DetailView):
   raise_exception = True
   model = Travel
   template_name = 'travels/detail.html'
 
+  def get_context_data(self, **kwargs):
+    context = super().get_context_data(**kwargs)
+    context['debug'] = DEBUG
+    context['active_app'] = 'travels'
+    return context
