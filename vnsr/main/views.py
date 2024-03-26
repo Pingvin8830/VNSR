e6545d0dd0a471a7b8ca5e0a2cc9f8d50440070a
anton <vlasov-ant@yandex.ru>
2024-03-26 15:52:37 +0300

    Add main authorization

--- vnsr/main/views.py
+++ vnsr/main/views.py
@@ -1,17 +1,19 @@
 from django.shortcuts import render
 from django.views import generic
 from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse
 
 from vnsr.settings import DEBUG
 
 # Create your views here.
-class Index(generic.TemplateView):
+class Index(LoginRequiredMixin, generic.TemplateView):
   template_name = 'main/index.html'
 
   def get_context_data(self, **kwargs):
     context = super().get_context_data(**kwargs)
     context['debug'] = DEBUG
+    context['active_app'] = 'main'
     return context
 
 class Login(LoginView):
