92c4545ea852486a3025823908ca64e0a7b29bc3
Anton <vlasov-ant@yandex.ru>
2024-04-07 01:24:27 +0300

    Create Dao class for model TravelState

--- Android/app/src/main/java/ru/sknt/vlasovnetwork/vnsr/models/travels/TravelStateDao.java
+++ Android/app/src/main/java/ru/sknt/vlasovnetwork/vnsr/models/travels/TravelStateDao.java
@@ -1,2 +1,27 @@
-package ru.sknt.vlasovnetwork.vnsr.models.travels;public interface TravelStateDao {
+package ru.sknt.vlasovnetwork.vnsr.models.travels;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface TravelStateDao {
+    @Query("SELECT * FROM travels_travelstate ORDER BY name")
+    List<TravelState> getAll();
+
+    @Insert
+    void create(TravelState travelState);
+
+    @Query("SELECT * FROM travels_travelstate WHERE id=:id")
+    TravelState find(int id);
+    @Query("SELECT * FROM travels_travelstate WHERE name=:name")
+    TravelState find(String name);
+    @Delete
+    void delete(TravelState travelState);
+
+    @Query("DELETE FROM travels_travelstate")
+    void truncate();
 }
