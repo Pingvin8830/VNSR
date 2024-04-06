eedf22045b252e774893b953ee93ee465f13e211
Anton <vlasov-ant@yandex.ru>
2024-04-07 01:21:58 +0300

    Create class VNSRDatabase

--- Android/app/src/main/java/ru/sknt/vlasovnetwork/vnsr/VNSRDatabase.java
+++ Android/app/src/main/java/ru/sknt/vlasovnetwork/vnsr/VNSRDatabase.java
@@ -1,2 +1,28 @@
-package ru.sknt.vlasovnetwork.vnsr;public class VNSRDatabase {
+package ru.sknt.vlasovnetwork.vnsr;
+
+import androidx.room.AutoMigration;
+import androidx.room.Database;
+import androidx.room.RenameTable;
+import androidx.room.RoomDatabase;
+import androidx.room.migration.AutoMigrationSpec;
+
+import ru.sknt.vlasovnetwork.vnsr.models.travels.TravelState;
+import ru.sknt.vlasovnetwork.vnsr.models.travels.TravelStateDao;
+
+@Database(
+        version = 4,
+        entities = {
+                TravelState.class
+        },
+        autoMigrations = {
+                @AutoMigration(from = 1, to = 2),
+                @AutoMigration(from = 2, to = 3),
+                @AutoMigration(from = 3, to = 4, spec = VNSRDatabase.Migration3to4.class)
+        }
+)
+public abstract class VNSRDatabase extends RoomDatabase {
+    @RenameTable(fromTableName = "TravelState", toTableName = "travels_travelstate")
+    static class Migration3to4 implements AutoMigrationSpec { }
+
+    public abstract TravelStateDao travelStateDao();
 }
