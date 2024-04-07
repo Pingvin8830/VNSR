8b0871422e955152d689ebe8a4cbb950ff522d41
Anton <vlasov-ant@yandex.ru>
2024-04-07 18:40:11 +0300

    Change class region to room model

--- Android/app/src/main/java/ru/sknt/vlasovnetwork/vnsr/models/kladr/Region.java
+++ Android/app/src/main/java/ru/sknt/vlasovnetwork/vnsr/models/kladr/Region.java
@@ -1,8 +1,37 @@
 package ru.sknt.vlasovnetwork.vnsr.models.kladr;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.Index;
+import androidx.room.PrimaryKey;
+
+@Entity(
+        tableName = "kladr_region",
+        indices = {
+                @Index(
+                        value = {"code"},
+                        unique = true
+                ),
+                @Index(
+                        value = {"name"},
+                        unique = true
+                )
+        }
+)
 public class Region {
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    private int mId;
+    @ColumnInfo(name = "code")
     private final String mCode;
+    @ColumnInfo(name = "name")
     private final String mName;
+
+    public int getId() { return this.mId; }
+    public String getCode() { return this.mCode; }
+    public String getName() { return this.mName; }
+
+    public void setId(int id) { this.mId = id; }
 
     public Region (String code, String name) {
         this.mCode = code;
