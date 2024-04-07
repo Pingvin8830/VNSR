43698b38e3694e6a2c943d4f81e05cff5342dcf7
Anton <vlasov-ant@yandex.ru>
2024-04-08 00:28:54 +0300

    Change class Address to room model

--- Android/app/src/main/java/ru/sknt/vlasovnetwork/vnsr/models/kladr/Address.java
+++ Android/app/src/main/java/ru/sknt/vlasovnetwork/vnsr/models/kladr/Address.java
@@ -1,19 +1,61 @@
 package ru.sknt.vlasovnetwork.vnsr.models.kladr;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.Index;
+import androidx.room.PrimaryKey;
+
+@Entity(
+        tableName = "kladr_address",
+        indices = {
+                @Index(
+                        value = {"name"},
+                        unique = true
+                )
+        }
+)
 public class Address {
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    private int mId;
+    @ColumnInfo(name = "name")
     private final String mName;
-    private final Region mRegion;
-    private final City mCity;
-    private final Street mStreet;
+    @ColumnInfo(name = "region_id")
+    private int mRegionId;
+    @Ignore
+    private Region mRegion;
+    @ColumnInfo(name = "city_id")
+    private int mCityId;
+    @Ignore
+    private City mCity;
+    @ColumnInfo(name = "street_id")
+    private int mStreetId;
+    @Ignore
+    private Street mStreet;
+    @ColumnInfo(name = "house")
     private final String mHouse;
+    @ColumnInfo(name = "building")
     private final String mBuilding;
+    @ColumnInfo(name = "flat")
     private int mFlat;
 
-    public Address (String name, Region region, City city, Street street, String house, String building, int flat) {
+    public int getId() { return this.mId; }
+    public String getName() { return this.mName; }
+    public int getRegionId() { return this.mRegionId; }
+    public int getCityId() { return this.mCityId; }
+    public int getStreetId() { return this.mStreetId; }
+    public String getHouse() { return this.mHouse; }
+    public String getBuilding() { return this.mBuilding; }
+    public int getFlat() { return this.mFlat; }
+
+    public void setId(int id) { this.mId = id; }
+
+    public Address (String name, int regionId, int cityId, int streetId, String house, String building, int flat) {
         this.mName = name;
-        this.mRegion = region;
-        this.mCity = city;
-        this.mStreet = street;
+        this.mRegionId = regionId;
+        this.mCityId = cityId;
+        this.mStreetId = streetId;
         this.mHouse = house;
         this.mBuilding = building;
         this.mFlat = flat;
