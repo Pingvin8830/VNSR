1bdf4348d512ad109dafdd6183a40202eb4e501e
Anton <vlasov-ant@yandex.ru>
2024-04-07 23:06:11 +0300

    Change class street to room module

--- Android/app/src/main/java/ru/sknt/vlasovnetwork/vnsr/models/kladr/Street.java
+++ Android/app/src/main/java/ru/sknt/vlasovnetwork/vnsr/models/kladr/Street.java
@@ -1,11 +1,40 @@
 package ru.sknt.vlasovnetwork.vnsr.models.kladr;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.Index;
+import androidx.room.PrimaryKey;
+
+@Entity(
+        tableName = "kladr_street",
+        indices = {
+                @Index(
+                        value = {"type_id", "name"},
+                        unique = true
+                )
+        }
+)
 public class Street {
-    private final StreetType mType;
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    private int mId;
+
+    @ColumnInfo(name = "type_id")
+    private int mTypeId;
+    @Ignore
+    private StreetType mType;
+    @ColumnInfo(name = "name")
     private final String mName;
 
-    public Street (StreetType type, String name) {
-        this.mType = type;
+    public int getId() { return this.mId; }
+    public int getTypeId() { return this.mTypeId; }
+    public String getName() { return this.mName; }
+
+    public void setId(int id) { this.mId = id; }
+
+    public Street (int typeId, String name) {
+        this.mTypeId = typeId;
         this.mName = name;
     }
 }
