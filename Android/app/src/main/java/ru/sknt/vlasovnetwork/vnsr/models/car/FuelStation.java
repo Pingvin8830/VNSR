683fca7fc54b7714211fc5e6c7bd03c75c0b16d1
Anton <vlasov-ant@yandex.ru>
2024-04-08 01:22:24 +0300

    Chenge class fuel station to room model

--- Android/app/src/main/java/ru/sknt/vlasovnetwork/vnsr/models/car/FuelStation.java
+++ Android/app/src/main/java/ru/sknt/vlasovnetwork/vnsr/models/car/FuelStation.java
@@ -1,17 +1,43 @@
 package ru.sknt.vlasovnetwork.vnsr.models.car;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+
 import ru.sknt.vlasovnetwork.vnsr.models.kladr.Address;
 
+@Entity(
+        tableName = "car_fuelstation"
+)
 public class FuelStation {
-    private final String mCompany;
-    private final String mName;
-    private final String mPhone;
-    private final Address mAddress;
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    private int mId;
 
-    public FuelStation(String company, String name, String phone, Address address) {
+    @ColumnInfo(name = "company")
+    private final String mCompany;
+    @ColumnInfo(name = "name")
+    private final String mName;
+    @ColumnInfo(name = "phone")
+    private final String mPhone;
+    @ColumnInfo(name = "address_id")
+    private int mAddressId;
+    @Ignore
+    private Address mAddress;
+
+    public int getId() { return this.mId; }
+    public String getCompany() { return this.mCompany; }
+    public String getName() { return this.mCompany; }
+    public String getPhone() { return this.mPhone; }
+    public int getAddressId() { return this.mAddressId; }
+
+    public void setId(int id) { this.mId = id; }
+
+    public FuelStation (String company, String name, String phone, int addressId) {
         this.mCompany = company;
         this.mName = name;
         this.mPhone = phone;
-        this.mAddress = address;
+        this.mAddressId = addressId;
     }
 }
