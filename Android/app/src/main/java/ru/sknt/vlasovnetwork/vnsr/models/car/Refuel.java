8d3450c75ceeee0f131e95b75da0848d2c6bd5b2
Anton <vlasov-ant@yandex.ru>
2024-04-07 18:18:11 +0300

    Change class refuel to room model

--- Android/app/src/main/java/ru/sknt/vlasovnetwork/vnsr/models/car/Refuel.java
+++ Android/app/src/main/java/ru/sknt/vlasovnetwork/vnsr/models/car/Refuel.java
@@ -1,27 +1,75 @@
 package ru.sknt.vlasovnetwork.vnsr.models.car;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+
+@Entity(
+        tableName = "car_refuel"
+)
 public class Refuel {
-    private final FuelStation mFuelStation;
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    private int mId;
+
+    @ColumnInfo(name = "fuel_station_id")
+    private int mFuelStationId;
+    @Ignore
+    private FuelStation mFuelStation;
+    @ColumnInfo(name = "check_number")
     private int mCheckNumber;
+    @ColumnInfo(name = "datetime")
     private long mDateTime;
+    @ColumnInfo(name = "trk")
     private int mTrk = 3;
-    private final Fuel mFuel;
+    @ColumnInfo(name = "fuel_id")
+    private int mFuelId;
+    @Ignore
+    private Fuel mFuel;
+    @ColumnInfo(name = "count")
     private float mCount;
+    @ColumnInfo(name = "price")
     private float mPrice;
+    @ColumnInfo(name = "cost")
     private float mCost;
+    @ColumnInfo(name = "distance_reserve")
     private int mDistanceReserve = 500;
+    @ColumnInfo(name = "fuel_consumption_avg")
     private float mFuelConsumptionAvg = 10f;
+    @ColumnInfo(name = "odometer")
     private int mOdometer;
+    @ColumnInfo(name = "distance")
     private float mDistance;
+    @ColumnInfo(name = "fuel_consumption")
     private float mFuelConsumption = 10f;
+    @ColumnInfo(name = "timedelta")
     private final String mTimedelta;
 
+    public int getId() { return this.mId; }
+    public int getFuelStationId() { return this.mFuelStationId; }
+    public int getCheckNumber() { return this.mCheckNumber; }
+    public long getDateTime() { return this.mDateTime; }
+    public int getTrk() { return this.mTrk; }
+    public int getFuelId() { return this.mFuelId; }
+    public float getCount() { return this.mCount; }
+    public float getPrice() { return this.mPrice; }
+    public float getCost() { return this.mCost; }
+    public int getDistanceReserve() { return this.mDistanceReserve; }
+    public float getFuelConsumptionAvg() { return this.mFuelConsumptionAvg; }
+    public int getOdometer() { return this.mOdometer; }
+    public float getDistance() { return this.mDistance; }
+    public float getFuelConsumption() { return this.mFuelConsumption; }
+    public String getTimedelta() { return this.mTimedelta; }
+
+    public void setId(int id) { this.mId = id; }
+
     public Refuel(
-            FuelStation fuelStation,
+            int fuelStationId,
             int checkNumber,
             long dateTime,
             int trk,
-            Fuel fuel,
+            int fuelId,
             float count,
             float price,
             float cost,
@@ -32,16 +80,16 @@ public class Refuel {
             float fuelConsumption,
             String timedelta
     ) {
-        this.mFuelStation = fuelStation;
+        this.mFuelStationId = fuelStationId;
         this.mCheckNumber = checkNumber;
         this.mDateTime = dateTime;
         this.mTrk = trk;
-        this.mFuel = fuel;
+        this.mFuelId = fuelId;
         this.mCount = count;
         this.mPrice = price;
         this.mCost = cost;
         this.mDistanceReserve = distanceReserve;
-        this.mFuelConsumptionAvg = fuelConsumption;
+        this.mFuelConsumptionAvg = fuelConsumptionAvg;
         this.mOdometer = odometer;
         this.mDistance = distance;
         this.mFuelConsumption = fuelConsumption;
