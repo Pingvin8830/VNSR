51efdd3ff8da6327e2f99dacc9ca3a4d8601d9d4
Anton <vlasov-ant@yandex.ru>
2024-04-07 13:17:38 +0300

    Add travel table and travel dao in database class

--- Android/app/src/main/java/ru/sknt/vlasovnetwork/vnsr/VNSRDatabase.java
+++ Android/app/src/main/java/ru/sknt/vlasovnetwork/vnsr/VNSRDatabase.java
@@ -1,28 +1,31 @@
 package ru.sknt.vlasovnetwork.vnsr;
 
-import androidx.room.AutoMigration;
 import androidx.room.Database;
-import androidx.room.RenameTable;
 import androidx.room.RoomDatabase;
-import androidx.room.migration.AutoMigrationSpec;
 
+import ru.sknt.vlasovnetwork.vnsr.daos.TravelDao;
+import ru.sknt.vlasovnetwork.vnsr.models.travels.Travel;
 import ru.sknt.vlasovnetwork.vnsr.models.travels.TravelState;
-import ru.sknt.vlasovnetwork.vnsr.models.travels.TravelStateDao;
+import ru.sknt.vlasovnetwork.vnsr.daos.TravelStateDao;
 
 @Database(
-        version = 4,
+        version = 1,
         entities = {
-                TravelState.class
-        },
-        autoMigrations = {
-                @AutoMigration(from = 1, to = 2),
-                @AutoMigration(from = 2, to = 3),
-                @AutoMigration(from = 3, to = 4, spec = VNSRDatabase.Migration3to4.class)
+                TravelState.class,
+                Travel.class
         }
+//        autoMigrations = {
+//                @AutoMigration(from = 1, to = 2),
+//                @AutoMigration(from = 2, to = 3),
+//                @AutoMigration(from = 3, to = 4, spec = VNSRDatabase.Migration3to4.class),
+//                @AutoMigration(from = 4, to = 5),
+//                @AutoMigration(from = 5, to = 6)
+//        }
 )
 public abstract class VNSRDatabase extends RoomDatabase {
-    @RenameTable(fromTableName = "TravelState", toTableName = "travels_travelstate")
-    static class Migration3to4 implements AutoMigrationSpec { }
+    //@RenameTable(fromTableName = "TravelState", toTableName = "travels_travelstate")
+    //static class Migration3to4 implements AutoMigrationSpec { }
 
     public abstract TravelStateDao travelStateDao();
+    public abstract TravelDao travelDao();
 }
