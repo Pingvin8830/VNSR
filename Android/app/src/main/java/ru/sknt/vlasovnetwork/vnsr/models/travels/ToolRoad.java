35b77808870f51c62d73789f435ee1d0157f9ce6
Anton <vlasov-ant@yandex.ru>
2024-04-07 14:28:14 +0300

    Change class ToolRoad as Room model

--- Android/app/src/main/java/ru/sknt/vlasovnetwork/vnsr/models/travels/ToolRoad.java
+++ Android/app/src/main/java/ru/sknt/vlasovnetwork/vnsr/models/travels/ToolRoad.java
@@ -1,14 +1,41 @@
 package ru.sknt.vlasovnetwork.vnsr.models.travels;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+
+@Entity (
+        tableName = "travels_tollroad"
+)
 public class ToolRoad {
-    private final Travel mTravel;
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    private int mId;
+    @ColumnInfo(name = "travel_id")
+    private int mTravelId;
+    @Ignore
+    private Travel mTravel;
+    @ColumnInfo(name = "name")
     private final String mName;
+    @ColumnInfo(name = "start")
     private final String mStart;
+    @ColumnInfo(name = "end")
     private final String mEnd;
+    @ColumnInfo(name = "price")
     private float mPrice;
 
-    public ToolRoad (Travel travel, String name, String start, String end, float price) {
-        this.mTravel = travel;
+    public int getId() { return this.mId; }
+    public int getTravelId() { return this.mTravelId; }
+    public String getName() { return this.mName; }
+    public String getStart() { return this.mStart; }
+    public String getEnd() { return this.mEnd; }
+    public float getPrice() { return this.mPrice; }
+
+    public void setId(int id) { this.mId = id; }
+
+    public ToolRoad (int travelId, String name, String start, String end, float price) {
+        this.mTravelId = travelId;
         this.mName = name;
         this.mStart = start;
         this.mEnd = end;
