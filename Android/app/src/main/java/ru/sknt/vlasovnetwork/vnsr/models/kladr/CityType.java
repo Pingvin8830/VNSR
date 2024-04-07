8e38098ccb9ec391855c9690f207674393f00fc7
Anton <vlasov-ant@yandex.ru>
2024-04-07 23:47:49 +0300

    Change class City Type to room model

--- Android/app/src/main/java/ru/sknt/vlasovnetwork/vnsr/models/kladr/CityType.java
+++ Android/app/src/main/java/ru/sknt/vlasovnetwork/vnsr/models/kladr/CityType.java
@@ -1,11 +1,41 @@
 package ru.sknt.vlasovnetwork.vnsr.models.kladr;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.Index;
+import androidx.room.PrimaryKey;
+
+@Entity(
+        tableName = "kladr_citytype",
+        indices = {
+                @Index(
+                        value = {"short"},
+                        unique = true
+                ),
+                @Index(
+                        value = {"name"},
+                        unique = true
+                )
+        }
+)
 public class CityType {
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    private int mId;
+
+    @ColumnInfo(name = "name")
     private final String mName;
+    @ColumnInfo(name = "short")
     private final String mShort;
 
-    public CityType (String name, String shortName) {
+    public int getId() { return this.mId; }
+    public String getName() { return this.mName; }
+    public String getShort() { return this.mShort; }
+
+    public void setId(int id) { this.mId = id; }
+
+    public CityType (String name, String mShort) {
         this.mName = name;
-        this.mShort = shortName;
+        this.mShort = mShort;
     }
 }
