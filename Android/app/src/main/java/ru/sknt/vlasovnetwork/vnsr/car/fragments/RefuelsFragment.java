f99682074e9ce65b93406023d9b8e87e2fd5b37f
Anton <vlasov-ant@yandex.ru>
2024-04-12 01:24:42 +0300

    Create car refuels fragment

---
+++ Android/app/src/main/java/ru/sknt/vlasovnetwork/vnsr/car/fragments/RefuelsFragment.java
@@ -0,0 +1,18 @@
+package ru.sknt.vlasovnetwork.vnsr.car.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import ru.sknt.vlasovnetwork.vnsr.R;
+
+public class RefuelsFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.car_refuels, container, false);
+        return v;
+    }
+}
