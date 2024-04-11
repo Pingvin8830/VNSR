ff4e407da05e20bb00b686f981602232142ac336
Anton <vlasov-ant@yandex.ru>
2024-04-12 00:59:24 +0300

    Create kladr streets fragment

---
+++ Android/app/src/main/java/ru/sknt/vlasovnetwork/vnsr/kladr/fragments/StreetsFragment.java
@@ -0,0 +1,18 @@
+package ru.sknt.vlasovnetwork.vnsr.kladr.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import ru.sknt.vlasovnetwork.vnsr.R;
+
+public class StreetsFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.kladr_streets, container, false);
+        return v;
+    }
+}
