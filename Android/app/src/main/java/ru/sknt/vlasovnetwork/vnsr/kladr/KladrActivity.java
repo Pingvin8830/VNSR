ba79ad07541850c82038ef1d998a69520a97703e
Anton <vlasov-ant@yandex.ru>
2024-04-12 00:57:22 +0300

    Create kladr activity

---
+++ Android/app/src/main/java/ru/sknt/vlasovnetwork/vnsr/kladr/KladrActivity.java
@@ -0,0 +1,80 @@
+package ru.sknt.vlasovnetwork.vnsr.kladr;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.navigation.NavigationView;
+
+import ru.sknt.vlasovnetwork.vnsr.R;
+import ru.sknt.vlasovnetwork.vnsr.kladr.fragments.AddressesFragment;
+import ru.sknt.vlasovnetwork.vnsr.kladr.fragments.CityTypesFragment;
+import ru.sknt.vlasovnetwork.vnsr.kladr.fragments.CityesFragment;
+import ru.sknt.vlasovnetwork.vnsr.kladr.fragments.RegionsFragment;
+import ru.sknt.vlasovnetwork.vnsr.kladr.fragments.StreetTypesFragment;
+import ru.sknt.vlasovnetwork.vnsr.kladr.fragments.StreetsFragment;
+
+public class KladrActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+    private DrawerLayout mDrawer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kladr);
+
+        mDrawer = findViewById(R.id.drawer_layout);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+
+        setSupportActionBar(toolbar);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, mDrawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
+        mDrawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mDrawer.isDrawerOpen(GravityCompat.START)) {
+            mDrawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Обработка щелчков элемента представления навигации здесь
+
+        // Создаём транзакцию
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        int id = item.getItemId();
+
+        if      (id == R.id.nav_kladr_regions)      { transaction.replace(R.id.fragmentHolder, new RegionsFragment()); }
+        else if (id == R.id.nav_kladr_cityes)       { transaction.replace(R.id.fragmentHolder, new CityesFragment()); }
+        else if (id == R.id.nav_kladr_streets)      { transaction.replace(R.id.fragmentHolder, new StreetsFragment()); }
+        else if (id == R.id.nav_kladr_addresses)    { transaction.replace(R.id.fragmentHolder, new AddressesFragment()); }
+        else if (id == R.id.nav_kladr_city_types)   { transaction.replace(R.id.fragmentHolder, new CityTypesFragment()); }
+        else if (id == R.id.nav_kladr_street_types) { transaction.replace(R.id.fragmentHolder, new StreetTypesFragment()); }
+        else if (id == R.id.nav_back) { finish(); }
+
+        // Просим Android запоминать, какие пункты меню выбрал пользователь
+        //transaction.addToBackStack(null);
+
+        // Реализуем изменение
+        transaction.commit();
+
+        mDrawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
