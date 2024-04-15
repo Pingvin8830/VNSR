0f1f1c66a91a9f2052c0df2da1dc86bf9c6dbe8b
Anton <vlasov-ant@yandex.ru>
2024-04-16 00:31:03 +0300

    Add ordering street types in dao

--- Android/app/src/main/java/ru/sknt/vlasovnetwork/vnsr/kladr/daos/StreetTypeDao.java
+++ Android/app/src/main/java/ru/sknt/vlasovnetwork/vnsr/kladr/daos/StreetTypeDao.java
@@ -1,4 +1,4 @@
-package ru.sknt.vlasovnetwork.vnsr.daos.kladr;
+package ru.sknt.vlasovnetwork.vnsr.kladr.daos;
 
 import androidx.room.Dao;
 import androidx.room.Delete;
@@ -7,11 +7,11 @@ import androidx.room.Query;
 
 import java.util.List;
 
-import ru.sknt.vlasovnetwork.vnsr.models.kladr.StreetType;
+import ru.sknt.vlasovnetwork.vnsr.kladr.models.StreetType;
 
 @Dao
 public interface StreetTypeDao {
-    @Query("SELECT * FROM kladr_streettype")
+    @Query("SELECT * FROM kladr_streettype ORDER BY name")
     List<StreetType> getAll();
 
     @Insert
