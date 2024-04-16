2b77c57a6631e07c65396811c1b4818a7f53c47e
Anton <vlasov-ant@yandex.ru>
2024-04-16 22:28:07 +0300

    Add foreign in street model

--- Android/app/src/main/java/ru/sknt/vlasovnetwork/vnsr/kladr/models/Street.java
+++ Android/app/src/main/java/ru/sknt/vlasovnetwork/vnsr/kladr/models/Street.java
@@ -26,19 +26,30 @@ public class Street {
     @ColumnInfo(name = "type_id")
     private int mTypeId;
     @Ignore
-    private StreetType mType;
+    private StreetType mStreetType;
     @ColumnInfo(name = "name")
     private final String mName;
 
     public int getId() { return this.mId; }
     public int getTypeId() { return this.mTypeId; }
     public String getName() { return this.mName; }
-    public StreetType getType(StreetTypeDao dao) { return dao.find(mTypeId); }
+    public StreetType getStreetType() { return mStreetType; }
 
     public void setId(int id) { this.mId = id; }
+    public void setStreetType(StreetTypeDao dao) { this.mStreetType = dao.find(mTypeId); }
 
     public Street (int typeId, String name) {
         this.mTypeId = typeId;
         this.mName = name;
+    }
+    public Street (StreetType type, String name) {
+        this.mStreetType = type;
+        this.mTypeId = mStreetType.getId();
+        this.mName = name;
+    }
+
+    @Override
+    public String toString() {
+        return getStreetType().getShort() + ". " + getName();
     }
 }
