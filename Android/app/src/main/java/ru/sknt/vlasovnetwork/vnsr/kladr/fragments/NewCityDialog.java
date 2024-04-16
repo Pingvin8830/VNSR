17ff395de44b559b3c6613620816a87b54e2c6ea
Anton <vlasov-ant@yandex.ru>
2024-04-16 22:55:59 +0300

    Add foreign in new city dialog

--- Android/app/src/main/java/ru/sknt/vlasovnetwork/vnsr/kladr/fragments/NewCityDialog.java
+++ Android/app/src/main/java/ru/sknt/vlasovnetwork/vnsr/kladr/fragments/NewCityDialog.java
@@ -16,6 +16,8 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
 
+import java.util.List;
+
 import ru.sknt.vlasovnetwork.vnsr.R;
 import ru.sknt.vlasovnetwork.vnsr.kladr.daos.CityTypeDao;
 import ru.sknt.vlasovnetwork.vnsr.kladr.models.City;
@@ -26,30 +28,16 @@ public class NewCityDialog extends DialogFragment implements View.OnClickListene
     private Animation mAnimError;
     private EditText mEdtxtName;
     private Spinner mSpnType;
-    private final CityTypeDao mCityTypeDao;
+    private final List<CityType> mCityTypes;
 
-    public NewCityDialog(CityTypeDao dao) { mCityTypeDao = dao; }
+    public NewCityDialog(CityTypeDao dao) {
+        mCityTypes = dao.getAll();
+    }
 
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         mAnimError = AnimationUtils.loadAnimation(getContext(),R.anim.error);
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        LayoutInflater inflater = getActivity().getLayoutInflater();
-        View dialogView = inflater.inflate(R.layout.kladr_new_city, null);
-        mTxtError = dialogView.findViewById(R.id.txtError);
-        mEdtxtName = dialogView.findViewById(R.id.edtxtName);
-        mSpnType = dialogView.findViewById(R.id.spnType);
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_item, mCityTypeDao.getAllNames());
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        mSpnType.setAdapter(adapter);
-
-        Button bttnAdd = dialogView.findViewById(R.id.bttnAdd);
-        Button bttnCancel = dialogView.findViewById(R.id.bttnCancel);
-
-        mTxtError.setVisibility(View.INVISIBLE);
-        builder.setView(dialogView).setMessage("Add a new city");
-
         mAnimError.setAnimationListener(
                 new Animation.AnimationListener() {
                     @Override
@@ -60,10 +48,24 @@ public class NewCityDialog extends DialogFragment implements View.OnClickListene
                     public void onAnimationRepeat(Animation animation) {}
                 }
         );
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.kladr_new_city, null);
+        mTxtError = dialogView.findViewById(R.id.txtError);
+        mEdtxtName = dialogView.findViewById(R.id.edtxtName);
+        mSpnType = dialogView.findViewById(R.id.spnType);
+        Button bttnAdd = dialogView.findViewById(R.id.bttnAdd);
+        Button bttnCancel = dialogView.findViewById(R.id.bttnCancel);
 
+        ArrayAdapter<CityType> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_item, mCityTypes);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mSpnType.setAdapter(adapter);
+
+        mTxtError.setVisibility(View.INVISIBLE);
         bttnCancel.setOnClickListener(this);
         bttnAdd.setOnClickListener(this);
 
+        builder.setView(dialogView).setMessage("Add a new city");
         return builder.create();
     }
 
@@ -72,13 +74,13 @@ public class NewCityDialog extends DialogFragment implements View.OnClickListene
         else if (v.getId() == R.id.bttnAdd) {
             // Создаём новый город
             String name = mEdtxtName.getText().toString();
-            CityType cityType = mCityTypeDao.find(mSpnType.getSelectedItem().toString());
+            CityType cityType = mCityTypes.get(mSpnType.getSelectedItemPosition());
 
             if (name.isEmpty()) {
                 mTxtError.setText("Bad name");
                 mTxtError.startAnimation(mAnimError);
             } else {
-                City newCity = new City(name, cityType.getId());
+                City newCity = new City(name, cityType);
 
                 // Получаем ссылку на Fragment
                 CityesFragment callingFragment = (CityesFragment) getActivity().getSupportFragmentManager().findFragmentByTag("cityes");
