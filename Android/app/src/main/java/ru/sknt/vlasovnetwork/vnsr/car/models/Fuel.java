569728ba3338884e6eb344f84f0f91127b4216b0
Anton <vlasov-ant@yandex.ru>
2024-04-19 21:32:19 +0300

    Upgrade fuel model

--- Android/app/src/main/java/ru/sknt/vlasovnetwork/vnsr/car/models/Fuel.java
+++ Android/app/src/main/java/ru/sknt/vlasovnetwork/vnsr/car/models/Fuel.java
@@ -3,14 +3,17 @@ package ru.sknt.vlasovnetwork.vnsr.car.models;
 import androidx.annotation.NonNull;
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
-import androidx.room.Ignore;
+import androidx.room.Index;
 import androidx.room.PrimaryKey;
 
-import ru.sknt.vlasovnetwork.vnsr.MainActivity;
-
-
 @Entity(
-        tableName = "car_fuel"
+        tableName = "car_fuel",
+        indices = {
+                @Index(
+                        value = {"name"},
+                        unique = true
+                )
+        }
 )
 public class Fuel {
     @PrimaryKey(autoGenerate = true)
@@ -19,30 +22,13 @@ public class Fuel {
 
     @ColumnInfo(name = "name")
     private final String mName;
-    @ColumnInfo(name = "fuel_station_id")
-    private final int mFuelStationId;
-    @Ignore
-    private FuelStation mFuelStation;
 
     public int getId() { return this.mId; }
     public String getName() { return this.mName; }
-    public int getFuelStationId() { return this.mFuelStationId; }
-    public FuelStation getFuelStation() { return this.mFuelStation; }
 
     public void setId(int id) { this.mId = id; }
-    public void setFuelStation() { this.mFuelStation = MainActivity.FuelStationDao.find(this.mFuelStationId); }
 
-    public Fuel(String name, int fuelStationId) {
-        this.mName = name;
-        this.mFuelStationId = fuelStationId;
-        this.setFuelStation();
-    }
-    public Fuel(String name, FuelStation fuelStation) {
-        this.mName = name;
-        this.mFuelStation = fuelStation;
-        this.mFuelStationId = fuelStation.getId();
-    }
-
+    public Fuel (String name) { this.mName = name; }
     @NonNull
     @Override
     public String toString() { return this.mName; }
