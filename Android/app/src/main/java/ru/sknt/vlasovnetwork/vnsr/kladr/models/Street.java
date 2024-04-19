bafc96a6f822a8873abf20b4c198f9cf8c71ae71
Anton <vlasov-ant@yandex.ru>
2024-04-19 22:04:59 +0300

    Cleaning

--- Android/app/src/main/java/ru/sknt/vlasovnetwork/vnsr/kladr/models/Street.java
+++ Android/app/src/main/java/ru/sknt/vlasovnetwork/vnsr/kladr/models/Street.java
@@ -1,12 +1,10 @@
 package ru.sknt.vlasovnetwork.vnsr.kladr.models;
 
+import androidx.annotation.NonNull;
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
-import androidx.room.Ignore;
 import androidx.room.Index;
 import androidx.room.PrimaryKey;
-
-import ru.sknt.vlasovnetwork.vnsr.MainActivity;
 
 @Entity(
         tableName = "kladr_street",
@@ -22,36 +20,22 @@ public class Street {
     @ColumnInfo(name = "id")
     private int mId;
 
-    //@ColumnInfo(name = "type_id")
-    //private int mTypeId;
-    //@Ignore
     @ColumnInfo(name = "type_id")
-    private StreetType mStreetType;
+    private final  StreetType mStreetType;
     @ColumnInfo(name = "name")
     private final String mName;
 
     public int getId() { return this.mId; }
-    //public int getTypeId() { return this.mTypeId; }
     public String getName() { return this.mName; }
     public StreetType getStreetType() { return mStreetType; }
 
     public void setId(int id) { this.mId = id; }
-    //public void setStreetType() { this.mStreetType = MainActivity.StreetTypeDao.find(mTypeId); }
-
-    /*public Street (int typeId, String name) {
-        this.mTypeId = typeId;
-        this.mName = name;
-    }*/
-    /*public Street (StreetType type, String name) {
-        this.mStreetType = type;
-        this.mTypeId = mStreetType.getId();
-        this.mName = name;
-    }*/
     public Street (String name, StreetType streetType) {
         this.mName = name;
         this.mStreetType = streetType;
     }
 
+    @NonNull
     @Override
     public String toString() {
         return getStreetType().getShort() + ". " + getName();
