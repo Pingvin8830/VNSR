c6aaacb115cc237ea0d7944b19c95360d7298257
Anton <vlasov-ant@yandex.ru>
2024-04-19 21:29:39 +0300

    Upgrade new fuel dialog for new model

--- Android/app/src/main/java/ru/sknt/vlasovnetwork/vnsr/car/fragments/NewFuelDialog.java
+++ Android/app/src/main/java/ru/sknt/vlasovnetwork/vnsr/car/fragments/NewFuelDialog.java
@@ -1,51 +1,32 @@
 package ru.sknt.vlasovnetwork.vnsr.car.fragments;
 
-import android.widget.ArrayAdapter;
 import android.widget.EditText;
-import android.widget.Spinner;
 
-import java.util.List;
-
-import ru.sknt.vlasovnetwork.vnsr.MainActivity;
 import ru.sknt.vlasovnetwork.vnsr.NewObjectDialog;
 import ru.sknt.vlasovnetwork.vnsr.R;
 import ru.sknt.vlasovnetwork.vnsr.car.models.Fuel;
-import ru.sknt.vlasovnetwork.vnsr.car.models.FuelStation;
 
 public class NewFuelDialog extends NewObjectDialog {
     private EditText mEdtxtName;
-    private Spinner mSpnFuelStation;
-    private List<FuelStation> mFuelStations;
     private String mName;
-    private FuelStation mFuelStation;
 
     @Override
-    protected void setObjectsLists() { mFuelStations = MainActivity.FuelStationDao.getAll(); }
+    protected void setObjectsLists() {}
 
     @Override
     protected int getLayoutCode() { return R.layout.car_new_fuel; }
 
     @Override
-    protected void getDataViews() {
-        mEdtxtName = mDialogView.findViewById(R.id.edtxtName);
-        mSpnFuelStation = mDialogView.findViewById(R.id.spnFuelStation);
-    }
+    protected void getDataViews() { mEdtxtName = mDialogView.findViewById(R.id.edtxtName); }
 
     @Override
-    protected void setAdapters() {
-        ArrayAdapter<FuelStation> fuelStationAdapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_item, mFuelStations);
-        fuelStationAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        mSpnFuelStation.setAdapter(fuelStationAdapter);
-    }
+    protected void setAdapters() {}
 
     @Override
     protected String getDialogMessageText() { return "Add a new fuel"; }
 
     @Override
-    protected void setData() {
-        mName = mEdtxtName.getText().toString();
-        mFuelStation = mFuelStations.get(mSpnFuelStation.getSelectedItemPosition());
-    }
+    protected void setData() { mName = mEdtxtName.getText().toString(); }
 
     @Override
     protected String getErrorText() {
@@ -56,7 +37,7 @@ public class NewFuelDialog extends NewObjectDialog {
 
     @Override
     protected void createObject() {
-        Fuel newFuel = new Fuel(mName, mFuelStation);
+        Fuel newFuel = new Fuel(mName);
         FuelsFragment callingFragment = (FuelsFragment) getActivity().getSupportFragmentManager().findFragmentByTag("fuels");
         callingFragment.createNewFuel(newFuel);
     }
