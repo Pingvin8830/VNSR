7164e179377563fa4cee467b7a905d4758fdbe38
Anton <vlasov-ant@yandex.ru>
2024-04-19 21:33:14 +0300

    Upgrade fuel station model

--- Android/app/src/main/java/ru/sknt/vlasovnetwork/vnsr/car/models/FuelStation.java
+++ Android/app/src/main/java/ru/sknt/vlasovnetwork/vnsr/car/models/FuelStation.java
@@ -3,14 +3,19 @@ package ru.sknt.vlasovnetwork.vnsr.car.models;
 import androidx.annotation.NonNull;
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
-import androidx.room.Ignore;
+import androidx.room.Index;
 import androidx.room.PrimaryKey;
 
-import ru.sknt.vlasovnetwork.vnsr.MainActivity;
 import ru.sknt.vlasovnetwork.vnsr.kladr.models.Address;
 
 @Entity(
-        tableName = "car_fuelstation"
+        tableName = "car_fuelstation",
+        indices = {
+                @Index(
+                        value = {"company", "number"},
+                        unique = true
+                )
+        }
 )
 public class FuelStation {
     @PrimaryKey(autoGenerate = true)
@@ -19,46 +24,29 @@ public class FuelStation {
 
     @ColumnInfo(name = "company")
     private final String mCompany;
-    @ColumnInfo(name = "name")
-    private final String mName;
+    @ColumnInfo(name="number")
+    private final String mNumber;
     @ColumnInfo(name = "phone")
     private final String mPhone;
     @ColumnInfo(name = "address_id")
-    private final int mAddressId;
-    @Ignore
-    private Address mAddress;
+    private final Address mAddress;
 
     public int getId() { return this.mId; }
     public String getCompany() { return this.mCompany; }
-    public String getName() { return this.mName; }
+    public String getNumber() { return this.mNumber; }
     public String getPhone() { return this.mPhone; }
-    public int getAddressId() { return this.mAddressId; }
     public Address getAddress() { return this.mAddress; }
 
     public void setId(int id) { this.mId = id; }
-    public void setAddress() {
-        this.mAddress = MainActivity.AddressDao.find(this.mAddressId);
-        this.mAddress.setRegion();
-        this.mAddress.setCity();
-        this.mAddress.setStreet();
-    }
 
-    public FuelStation (String company, String name, String phone, int addressId) {
+    public FuelStation (String company, String number, String phone, Address address) {
         this.mCompany = company;
-        this.mName = name;
-        this.mPhone = phone;
-        this.mAddressId = addressId;
-        this.setAddress();
-    }
-    public FuelStation (String company, String name, String phone, Address address) {
-        this.mCompany = company;
-        this.mName = name;
+        this.mNumber = number;
         this.mPhone = phone;
         this.mAddress = address;
-        this.mAddressId = address.getId();
     }
 
     @NonNull
     @Override
-    public String toString() { return this.getName(); }
+    public String toString() { return this.mCompany + " " + this.mNumber; }
 }
