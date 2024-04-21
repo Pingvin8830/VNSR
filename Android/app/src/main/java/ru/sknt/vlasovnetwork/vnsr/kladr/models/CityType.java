3abae24f456e986a9bc0f4760a582286f3ff44f1
Anton <vlasov-ant@yandex.ru>
2024-04-21 21:21:22 +0300

    Create method toJson in city type model

--- Android/app/src/main/java/ru/sknt/vlasovnetwork/vnsr/kladr/models/CityType.java
+++ Android/app/src/main/java/ru/sknt/vlasovnetwork/vnsr/kladr/models/CityType.java
@@ -6,6 +6,9 @@ import androidx.room.Entity;
 import androidx.room.Index;
 import androidx.room.PrimaryKey;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 @Entity(
         tableName = "kladr_citytype",
         indices = {
@@ -43,4 +46,13 @@ public class CityType {
     @NonNull
     @Override
     public String toString() { return getName(); }
+    public JSONObject toJson() throws JSONException {
+        JSONObject res = new JSONObject();
+        res
+                .put("object", "CityType")
+                .put("id", this.getId())
+                .put("name", this.getName())
+                .put("short", this.getShort());
+        return res;
+    }
 }
