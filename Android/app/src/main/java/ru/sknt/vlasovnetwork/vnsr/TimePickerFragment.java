064455a5d64f81ba23cdd4597661bbbdae74b4b5
Anton <vlasov-ant@yandex.ru>
2024-04-30 23:05:52 +0300

    Change dialog case time

--- Android/app/src/main/java/ru/sknt/vlasovnetwork/vnsr/TimePickerFragment.java
+++ Android/app/src/main/java/ru/sknt/vlasovnetwork/vnsr/TimePickerFragment.java
@@ -14,13 +14,20 @@ import java.util.Calendar;
 
 import ru.sknt.vlasovnetwork.vnsr.car.fragments.NewRefuelDialog;
 import ru.sknt.vlasovnetwork.vnsr.travels.fragments.NewPointDialog;
+import ru.sknt.vlasovnetwork.vnsr.travels.fragments.NewTravelDialog;
 
 public class TimePickerFragment extends DialogFragment implements TimePickerDialog.OnTimeSetListener {
     private final String TAG;
+    private FormatedDate mRes;
 
     public TimePickerFragment (String tag) {
         super();
         TAG = tag;
+    }
+    public TimePickerFragment(String tag, FormatedDate res) {
+        super();
+        TAG = tag;
+        mRes = res;
     }
 
     @NonNull
@@ -38,20 +45,34 @@ public class TimePickerFragment extends DialogFragment implements TimePickerDial
     public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
         // Do something with the time the user picks.
         FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+        mRes.setHours(hourOfDay);
+        mRes.setMinutes(minute);
+        mRes.setSeconds(0);
         switch (TAG) {
             case "new_refuel":
                 NewRefuelDialog refuelDialog = (NewRefuelDialog) fragmentManager.findFragmentByTag(TAG);
                 assert refuelDialog != null;
-                refuelDialog.setHour(hourOfDay);
-                refuelDialog.setMinute(minute);
-                refuelDialog.setDateTimeButtonText();
+                refuelDialog.setDateTime(mRes);
                 break;
-            case "new_point":
-                NewPointDialog pointDialog = (NewPointDialog) fragmentManager.findFragmentByTag(TAG);
+            case "new_point_arrival":
+                NewPointDialog pointDialog = (NewPointDialog) fragmentManager.findFragmentByTag("new_point");
                 assert pointDialog != null;
-                pointDialog.setHour(hourOfDay);
-                pointDialog.setMinute(minute);
-                pointDialog.setDateTimeButtonText();
+                pointDialog.setArrivalDateTime(mRes);
+                break;
+            case "new_point_departure":
+                NewPointDialog pointDialog1 = (NewPointDialog) fragmentManager.findFragmentByTag("new_point");
+                assert pointDialog1 != null;
+                pointDialog1.setDepartureDateTime(mRes);
+                break;
+            case "new_travel_start":
+                NewTravelDialog travelDialog = (NewTravelDialog) fragmentManager.findFragmentByTag("new_travel");
+                assert travelDialog != null;
+                travelDialog.setStartDateTime(mRes);
+                break;
+            case "new_travel_end":
+                NewTravelDialog travelDialog1 = (NewTravelDialog) fragmentManager.findFragmentByTag("new_travel");
+                assert travelDialog1 != null;
+                travelDialog1.setEndDateTime(mRes);
                 break;
         }
     }
