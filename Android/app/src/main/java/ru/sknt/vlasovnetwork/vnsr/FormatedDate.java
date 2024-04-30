7db743031899a4b52b6f6b9f77cd2182918fa448
Anton <vlasov-ant@yandex.ru>
2024-04-30 23:05:13 +0300

    Create constructores from values and json

--- Android/app/src/main/java/ru/sknt/vlasovnetwork/vnsr/FormatedDate.java
+++ Android/app/src/main/java/ru/sknt/vlasovnetwork/vnsr/FormatedDate.java
@@ -2,11 +2,33 @@ package ru.sknt.vlasovnetwork.vnsr;
 
 import androidx.annotation.NonNull;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.Date;
 
 public class FormatedDate extends Date {
     public FormatedDate(Long value) {
         setTime(value);
+    }
+
+    public FormatedDate(int year, int month, int day, int hour, int minute) {
+        setTime(0L);
+        setYear(year);
+        setMonth(month);
+        setDate(day);
+        setHours(hour);
+        setMinutes(minute);
+        setSeconds(0);
+    }
+    public FormatedDate(JSONObject data) throws JSONException {
+        setTime(0L);
+        setYear(data.getInt("year"));
+        setMonth(data.getInt("month")-1);
+        setDate(data.getInt("day"));
+        setHours(data.getInt("hour"));
+        setMinutes(data.getInt("minute"));
+        setSeconds(0);
     }
 
     @NonNull
