fba80be566d38fb942d3322a4dd7bc936b7a013a
Anton <vlasov-ant@yandex.ru>
2024-05-01 09:58:25 +0300

    Fix name

--- Android/app/src/main/java/ru/sknt/vlasovnetwork/vnsr/travels/fragments/TollroadsFragment.java
+++ Android/app/src/main/java/ru/sknt/vlasovnetwork/vnsr/travels/fragments/TollroadsFragment.java
@@ -9,7 +9,7 @@ import androidx.fragment.app.Fragment;
 
 import ru.sknt.vlasovnetwork.vnsr.R;
 
-public class ToolroadsFragment extends Fragment {
+public class TollroadsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.travels_toolroads, container, false);
